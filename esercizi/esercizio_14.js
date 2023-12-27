3e22b451dbaa296a7575721c0b0ac6c252d051f6
Francesco Orazini <horace.supertramp@gmail.com>
2023-12-27 16:54:31 +0100

    Fix typo

--- esercizi/esercizio_14.js
+++ esercizi/esercizio_14.js
@@ -2,7 +2,7 @@
 /***
  * Completare il codice seguente in modo che, ogni 2 secondi:
  * - Il valore di counter venga aumentato di uno
- * - Counter venga stampato a video
+ * - Counter venga stampato in console
  * Ma solo fino a quando il valore di counter è inferiore a 10
  */
 
